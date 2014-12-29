2618eb799fbbfe5af9501385808151f043aa9021
Andrea Vacondio <andrea.vacondio@gmail.com>
2014-12-29 17:51:35 +0100

    Removed PowerMock dependency.

--- src/test/java/org/sejda/impl/itext5/component/ITextOutlineSubsetProviderTest.java
+++ src/test/java/org/sejda/impl/itext5/component/ITextOutlineSubsetProviderTest.java
@@ -21,7 +21,7 @@ package org.sejda.impl.itext5.component;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
@@ -30,31 +30,24 @@ import java.util.Collection;
 import java.util.HashMap;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.sejda.impl.itext5.util.ITextUtils;
 import org.sejda.model.exception.TaskException;
 
 import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.SimpleBookmark;
 
 /**
  * 
  * @author Andrea Vacondio
  * 
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(SimpleBookmark.class)
 public class ITextOutlineSubsetProviderTest {
-
     private PdfReader reader;
 
-    @Before
-    public void setUp() {
-        reader = mock(PdfReader.class);
+    @After
+    public void tearDown() {
+        ITextUtils.nullSafeClosePdfReader(reader);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -75,40 +68,52 @@ public class ITextOutlineSubsetProviderTest {
             assertFalse(retList.isEmpty());
         } finally {
             IOUtils.closeQuietly(inputStream);
-            if (reader != null) {
-                reader.close();
-            }
+        }
+    }
+    
+    @Test
+    public void getOutlineUntillPageEmptyBookmarks() throws TaskException, IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream("pdf/test_no_outline.pdf");
+            reader = spy(new PdfReader(inputStream));
+            ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
+            verify(reader).getNumberOfPages();
+            victim.startPage(0);
+            Collection<HashMap<String, Object>> retList = victim.getOutlineUntillPage(1);
+            assertNotNull(retList);
+            assertTrue(retList.isEmpty());
+        } finally {
+            IOUtils.closeQuietly(inputStream);
         }
     }
 
-    @Test
-    public void getOutlineUntillPageEmptyBookmarks() throws TaskException {
-        PowerMockito.mockStatic(SimpleBookmark.class);
-        PowerMockito.when(SimpleBookmark.getBookmark(reader)).thenReturn(null);
-        ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
-        verify(reader).getNumberOfPages();
-        victim.startPage(0);
-        Collection<HashMap<String, Object>> retList = victim.getOutlineUntillPage(1);
-        assertNotNull(retList);
-        assertTrue(retList.isEmpty());
+    @Test(expected = TaskException.class)
+    public void getOutlineUntillPageNoStart() throws TaskException, IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream("pdf/test_no_outline.pdf");
+            reader = spy(new PdfReader(inputStream));
+            ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
+            verify(reader).getNumberOfPages();
+            victim.getOutlineUntillPage(1);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
     }
 
     @Test(expected = TaskException.class)
-    public void getOutlineUntillPageNoStart() throws TaskException {
-        PowerMockito.mockStatic(SimpleBookmark.class);
-        PowerMockito.when(SimpleBookmark.getBookmark(reader)).thenReturn(null);
-        ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
-        verify(reader).getNumberOfPages();
-        victim.getOutlineUntillPage(1);
-    }
-
-    @Test(expected = TaskException.class)
-    public void getOutlineUntillPageStartGTEnd() throws TaskException {
-        PowerMockito.mockStatic(SimpleBookmark.class);
-        PowerMockito.when(SimpleBookmark.getBookmark(reader)).thenReturn(null);
-        ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
-        verify(reader).getNumberOfPages();
-        victim.startPage(5);
-        victim.getOutlineUntillPage(1);
+    public void getOutlineUntillPageStartGTEnd() throws TaskException, IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream("pdf/test_no_outline.pdf");
+            reader = spy(new PdfReader(inputStream));
+            ITextOutlineSubsetProvider victim = new ITextOutlineSubsetProvider(reader);
+            verify(reader).getNumberOfPages();
+            victim.startPage(5);
+            victim.getOutlineUntillPage(1);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
     }
 }
---
+++ src/test/java/org/sejda/impl/itext5/component/split/PagesPdfSplitterTest.java
@@ -0,0 +1,87 @@
+/*
+ * Created on 24/nov/2011
+ * Copyright 2011 by Andrea Vacondio (andrea.vacondio@gmail.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */
+package org.sejda.impl.itext5.component.split;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sejda.impl.itext5.component.PdfCopier;
+import org.sejda.impl.itext5.util.ITextUtils;
+import org.sejda.model.exception.TaskException;
+import org.sejda.model.input.PdfSource;
+import org.sejda.model.parameter.AbstractSplitByPageParameters;
+import org.sejda.model.parameter.SimpleSplitParameters;
+import org.sejda.model.pdf.PdfVersion;
+import org.sejda.model.pdf.page.PredefinedSetOfPages;
+import org.sejda.model.task.NotifiableTaskMetadata;
+
+import com.itextpdf.text.pdf.PdfReader;
+
+/**
+ * @author Andrea Vacondio
+ * 
+ */
+public class PagesPdfSplitterTest {
+
+    private SimpleSplitParameters params = new SimpleSplitParameters(PredefinedSetOfPages.ALL_PAGES);
+    private PdfReader reader;
+    private PdfSource source;
+
+    @Before
+    public void setUp() {
+        source = mock(PdfSource.class);
+        when(source.getName()).thenReturn("name");
+        params.setSource(source);
+    }
+
+    @After
+    public void tearDown() {
+        ITextUtils.nullSafeClosePdfReader(reader);
+    }
+
+    @Test(expected = OutOfMemoryError.class)
+    // issue #80
+    public void testFinallyDoesntSwallowErrors() throws IOException, TaskException {
+        InputStream inputStream = null;
+        try {
+            inputStream = getClass().getClassLoader().getResourceAsStream("pdf/test_no_outline.pdf");
+            reader = new PdfReader(inputStream);
+            PagesPdfSplitter<AbstractSplitByPageParameters> victim = spy(new PagesPdfSplitter<AbstractSplitByPageParameters>(
+                    reader, params));
+            PdfCopier mockCopier = mock(PdfCopier.class);
+            doReturn(mockCopier).when(victim).openCopier(any(PdfReader.class), any(File.class), any(PdfVersion.class));
+            doThrow(new RuntimeException()).when(mockCopier).close();
+            doThrow(new OutOfMemoryError()).when(mockCopier).addPage(reader, 1);
+            NotifiableTaskMetadata taskMetadata = mock(NotifiableTaskMetadata.class);
+            victim.split(taskMetadata);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+    }
+}
